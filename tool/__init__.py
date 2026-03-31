820b5fb8b4b1c2029858fc8152ca3e18bb05fc62
heamin <heamin0603@naver.com>
2026-04-01 02:57:10 +0900

    feat/tool: __init__.py에 monitor, ocr 툴 등록

--- tool/__init__.py
+++ tool/__init__.py
@@ -1,7 +1,10 @@
 from . import filesystem_tool, system_monitor_tool, web_search_tool
+from .tools import monitor, ocr
 
 tools = [
     *filesystem_tool.tools,
     *system_monitor_tool.tools,
-    *web_search_tool.tools
+    *web_search_tool.tools,
+    *monitor.tools,
+    *ocr.tools,
 ]
